ae43e4b751006dc2b1d94d40d36d0545deac740a
Kimowarui <68160336+Kimowarui@users.noreply.github.com>
2023-12-08 09:29:59 +0800

    add dialogue bubble

---
+++ experiential-learning/Assets/Scripts/Spatula.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Spatula : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private RectTransform rectTransform;
+    private Image image;
+    private CanvasGroup canvasGroup;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Debug.Log("OnBeginDrap");
+        image.color = new Color32(255, 255, 255, 170);
+        transform.SetParent(GetComponentInParent<Canvas>().transform);
+        canvasGroup.blocksRaycasts = false;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Debug.Log("OnEndDrap");
+        image.color = new Color32(255, 255, 255, 255);
+        canvasGroup.blocksRaycasts = true;
+        // Destroy(gameObject);
+    }
+
+    void Start(){
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+}
--- experiential-learning/Assets/Scripts/Table_press.cs
+++ experiential-learning/Assets/Scripts/Table_press.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TablePress : MonoBehaviour, IDropHandler
+public class TablePress : MonoBehaviour
 {  
     private GameObject dialogBubble;
+    private Slider slider;  
+    private bool isReleaseValveLocked;
+    private double pressure;
+    private bool inProgress;
+    private bool hasTablet;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("OnStart");
         dialogBubble = transform.Find("DialogBubble").gameObject;
-        HideDialogBubble();
+        slider = GetComponentInChildren<Slider>(true);
+        Debug.Log(slider);
+        isReleaseValveLocked = false;
+        pressure = 0;
+        inProgress = false;
+        hasTablet = false;
     }
 
     // Update is called once per frame
@@ -22,19 +34,72 @@ public class TablePress : MonoBehaviour, IDropHandler
         
     }
 
-    public void OnDrop(PointerEventData eventData) {
-        Debug.Log("OnDrop");
+    public void ToggleIsReleaseValve() {
+        Powder powder = GetComponentInChildren<Powder>();
+        if (isReleaseValveLocked) pressure = 0;
+        isReleaseValveLocked = !isReleaseValveLocked;
+        Debug.Log("Release Valve:" + isReleaseValveLocked);
+    }
+
+
+    public void LowerPunch() {
+        Powder powder = GetComponentInChildren<Powder>();
+        if (isReleaseValveLocked) {
+            pressure += 0.25;
+        }
+        if (pressure >= 1 && powder && !inProgress) StartPressTablet();
+
+        Talk("Current Pressure:" + pressure + " ton");
+        Debug.Log("Current Pressure:" + pressure + " ton");
+    }
+
+    public void EjectTablet() {
+        if (hasTablet && pressure == 0) {
+            hasTablet = false;
+            inProgress = false;
+            Talk("Congrats! You successfully make your own tablet.");
+            Debug.Log("Congrats! You successfully make your own tablet.");
+        }
     }
 
     void OnMouseOver() {
-
-        if (Input.GetMouseButtonDown(0)) {
-            dialogBubble.SetActive(true);
-            Invoke("HideDialogBubble", 2f);
+        if (Input.GetMouseButtonDown(0) && !GetComponentInChildren<Powder>() && !inProgress) {
+            string message = "Feed it some powder for a magical transformation!";
+            Talk(message);
         }
     }
 
     private void HideDialogBubble() {
         dialogBubble.SetActive(false);
     }
+
+    private void StartPressTablet() {
+        inProgress = true;
+        Powder powder = GetComponentInChildren<Powder>();
+        Destroy(powder.gameObject);
+        StartCoroutine(SimulatePressTablet());
+    }
+
+    IEnumerator SimulatePressTablet() {
+        float progress = 0f;
+        slider.gameObject.SetActive(true);
+        while (progress < 1f) {
+            progress += Time.deltaTime / 2f;
+            slider.value = progress;
+            yield return null;
+        }
+        slider.gameObject.SetActive(false);
+        hasTablet = true;
+        Talk("Pressing Tablet Completed!");
+        Debug.Log("Pressing Tablet Completed");
+    }
+
+    private void Talk(string text) {
+        HideDialogBubble();
+        TMP_Text content = dialogBubble.GetComponentInChildren<TMP_Text>();
+        content.text = text;
+        dialogBubble.SetActive(true);
+        Invoke("HideDialogBubble", 2f);
+    }
+
 }
